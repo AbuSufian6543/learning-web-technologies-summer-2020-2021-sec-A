462a575898773a20cf313b695f5e659379135f55
AbuSufian6543 <sufian6543@gmail.com>
2021-06-13 10:40:56 +0600

    check array

---
+++ LAB_TASK_03_PHP_INTRO/search from array.php
@@ -0,0 +1,8 @@
+ $week = ["Sat", "Sun", "Mon", "Tues", "Wed"];
+
+        for ($i = 0; $i < count($week); $i++)  {
+			
+			if (count($week)== "Sun"){echo "matched";}
+			else {echo "Not yet";}
+     
+        }
