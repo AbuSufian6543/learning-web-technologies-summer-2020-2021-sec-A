21794c954e89bbb268c80b1df498913fba1a01b0
AbuSufian6543 <sufian6543@gmail.com>
2021-06-15 19:35:17 +0600

    T2_C

--- LAB_TASK_4_PHP_FORM/Task_2/C/form.php
+++ LAB_TASK_4_PHP_FORM/Task_2/C/form.php
@@ -1,14 +1,37 @@
+<?php
+	
+	$email = "";
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$email = $_REQUEST['email'];
+		if($email == ""){
+			echo "invalid email!";
+		}
+	}else{
+		echo 'invalid request';
+	}
+?>
 
-<!DOCTYPE html>
+
+
 <html>
 <head>
-	<title>Form Data</title>
+	<title>HTML Form Tag</title>
 </head>
 <body>
-	<form method="post" >
 
-		Username: <input type="text" name="username" value="<?php echo $username; ?>" />
-				<input type="submit" name="submit" value="Submit">
-	</form>
+		<form method="POST" >
+			<table>
+				<tr>
+					<td>Email</td></tr>
+					
+					<tr><td><input type="email" name="email" value="<?php echo $email; ?>" placeholder="type email"></td>
+				</tr>
+				<tr><td><input type="submit" name="submit" value="Submit"></td></tr>
+				
+			</table>
+		</form>
+	
 </body>
 </html>
