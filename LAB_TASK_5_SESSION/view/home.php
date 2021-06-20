c96a6c1feea7dbc1cb9bd23ad9442b0673a895c2
AbuSufian6543 <sufian6543@gmail.com>
2021-06-20 10:42:29 +0600

    re checked t41

--- LAB_TASK_5_SESSION/view/home.php
+++ LAB_TASK_5_SESSION/view/home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PUBLIC HOME</title>
+	<title>EDIT PROFILE</title>
 </head>
 <body>
 
@@ -34,14 +34,25 @@
 
 
 
+<?php
+	session_start();
+	if(isset($_SESSION['flag'])){
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+	<h1>Welcome home! <?=$_SESSION['user']['username']?> </h1>
+	<a href="logout.php">logout</a>
+</body>
+</html>
 
-
-
-
-
-
-
-
-
-
+<?php
+	
+	}else{
+		header('location: login.php');
+	}
+?>
